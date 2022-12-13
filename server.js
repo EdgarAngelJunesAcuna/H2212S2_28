da8c5fdba80827e95d423c35f99c6a7b12b1fc99
Edgar Angel Raziel Junes Acuña <edgar.junes@vallegrande.edu.pe>
2022-12-13 03:35:52 -0500

    aaaa

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const http = require('http');
+
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.render('menu.html'));
+//recursos
+app.use(express.static(__dirname,'/'));
+
+//Enrutamiento
+app.get('/', (req, res) => {res.render('menu.html');
+});
 
 app.listen(3000);
 console.log('Server on port 3000');
