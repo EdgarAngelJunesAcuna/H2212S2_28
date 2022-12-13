04c224fffe608cd45658563516c80e77f840fba0
Edgar Angel Raziel Junes Acuña <edgar.junes@vallegrande.edu.pe>
2022-12-13 07:27:49 -0500

    aaaa

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const express= require('express');
+const http= require('http');
+const express = require('express');
+const app = express();
 
+//recursos
+app.use(express.static(__dirname+'/'));
 
+//Enrutamiento
+app.get('/',(req,res) => {
+    res.sendFile("/home/ubuntu/projects/landing/menu.html")
+});
 
-const server = http.createServer((req, res) =>{
-    res.end('welcome')
-})
-
-server.listen(3000);
-console.log('Server on port 3000');
+app.listen(3000);
+console.log('server on port 3000');
