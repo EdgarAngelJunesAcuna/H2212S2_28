37ad60ccc457edafe34077298dba12c45ff557fd
Edgar Angel Raziel Junes Acuña <edgar.junes@vallegrande.edu.pe>
2022-12-14 13:50:15 -0500

    a

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname+'/'));
 
 //Enrutamiento
 app.get('/',(req,res) => {
-    res.sendFile("/home/ubuntu/Colegio-Asia-Lainding-Page/menu.html")
+    res.sendFile("/home/ubuntu/projects/landing/menu.html")
 });
 
 app.listen(3000);
